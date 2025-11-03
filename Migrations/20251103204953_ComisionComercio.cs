b3da25b7fdd0a56c8a9c6125333758aceae7e72c
Vicente <vsmx427@gmail.com>
2025-11-03 17:50:32 -0300

    Comision Comercio

---
+++ Migrations/20251103204953_ComisionComercio.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DeliveryYaBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ComisionComercio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "comision",
+                table: "comercio",
+                type: "decimal(10,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "comision",
+                table: "comercio");
+        }
+    }
+}
