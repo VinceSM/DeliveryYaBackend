fd694f6c761c0b77812ef41b8f17f6a78e829760
unknown <gabrielmuise765@gmail.com>
2025-11-17 13:15:34 -0300

    url program.cs

--- Program.cs
+++ Program.cs
@@ -119,4 +119,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.Urls.Add("http://localhost:5189");
+app.Urls.Add("http://192.168.1.40:5189"); // Mi IP
+app.Urls.Add("http://0.0.0.0:5189");
+
 app.Run();
